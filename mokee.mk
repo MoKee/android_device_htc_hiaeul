856973b01d2b2c14ebc6f7c8985ddb3fd9a51ecf
martincz <martincz@mokeedev.com>
2016-02-05 19:05:31 +0800

    hiaeul: mokee bringup

--- mokee.mk
+++ mokee.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/htc/hiaeul/full_hiaeul.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_hiaeul
+PRODUCT_NAME := mk_hiaeul
 BOARD_VENDOR := htc
 TARGET_VENDOR := htc
 PRODUCT_DEVICE := hiaeul
